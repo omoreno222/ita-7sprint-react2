cb6e23f8f51b0b10b26d44e8cea01c3798cb53ee
oscarmoreno222 <oscar.moreno222@gmail.com>
2023-06-26 13:46:02 +0200

    Ejercicio 3 inicio

---
+++ src/Componentes/FetMida.jsx/FetMida_styled copy.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const FetMidaEstilos = styled.div`
+  button {
+    margin: 0.2rem;
+    width: 2rem;
+    background-color: orange;
+    padding: 0.2rem;
+    border: solid orange 1px;
+    border-radius: 10px;
+    color: white;
+    font-size: 1.5rem;
+    font-weight: 600px;
+    text-align: center;
+  }
+
+  input {
+    width: 1.4rem;
+  }
+`;
+
+export default FetMidaEstilos;
--- src/Componentes/Panell/Panell.jsx
+++ src/Componentes/Panell/Panell.jsx
@@ -1,33 +1,60 @@
 import { ContenedorFormulari } from "../ContenedorFormulari/ContenedorFormulari_styled";
 
-const Panell = ({ paginas, idiomas, setPaginas, setIdiomas }) => (
-  <ContenedorFormulari>
-    <div>
-      <label htmlFor="paginas">Número de páginas</label>
+const Panell = ({ paginas, idiomas, setPaginas, setIdiomas }) => {
+  const incrementarPaginas = () => {
+    setPaginas(parseInt(paginas) + 1);
+  };
+
+  const decrementarPaginas = () => {
+    if (paginas > 0) {
+      setPaginas(parseInt(paginas) - 1);
+    }
+  };
+
+  const incrementarIdiomas = () => {
+    setIdiomas(parseInt(idiomas) + 1);
+  };
+
+  const decrementarIdiomas = () => {
+    if (idiomas > 0) {
+      setIdiomas(parseInt(idiomas) - 1);
+    }
+  };
+
+  return (
+    <ContenedorFormulari>
       <div>
-        <input
-          id="páginas"
-          type="text"
-          min="0"
-          value={paginas}
-          onChange={(event) => setPaginas(event.target.value)}
-        />
+        <label htmlFor="paginas">Número de páginas</label>
+        <div>
+          <button onClick={incrementarPaginas}>+</button>
+          <input
+            id="paginas"
+            type="text"
+            min="0"
+            value={paginas}
+            onChange={(event) => setPaginas(event.target.value)}
+          />
+          <button onClick={decrementarPaginas}>-</button>
+        </div>
       </div>
-    </div>
-    <br />
-    <div>
-      <label htmlFor="idiomas">Número de idiomas</label>
+      <br />
       <div>
-        <input
-          id="idiomas"
-          type="text"
-          min="0"
-          value={idiomas}
-          onChange={(event) => setIdiomas(event.target.value)}
-        />
+        <label htmlFor="idiomas">Número de idiomas</label>
+
+        <div>
+          <button onClick={incrementarIdiomas}>+</button>
+          <input
+            id="idiomas"
+            type="text"
+            min="0"
+            value={idiomas}
+            onChange={(event) => setIdiomas(event.target.value)}
+          />
+          <button onClick={decrementarIdiomas}>-</button>
+        </div>
       </div>
-    </div>
-  </ContenedorFormulari>
-);
+    </ContenedorFormulari>
+  );
+};
 
 export default Panell;
