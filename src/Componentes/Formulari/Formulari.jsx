67e15ce3ba256c73f7cb3093d3b2949362561fd1
oscarmoreno222 <oscar.moreno222@gmail.com>
2023-06-27 19:39:52 +0200

    Ejercicio 4. Problema contadores

--- src/Componentes/Formulari/Formulari.jsx
+++ src/Componentes/Formulari/Formulari.jsx
@@ -1,12 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Panell from "../Panell/Panell";
 import { ContenedorFormulari } from "../ContenedorFormulari/ContenedorFormulari_styled";
 
 const Formulario = () => {
-  const [precioFinal, setPrecioFinal] = useState(0);
-  const [mostrarPanell, setMostrarPanell] = useState(false);
-  const [nPaginas, setNPaginas] = useState(0);
-  const [nIdiomas, setNIdiomas] = useState(0);
+  const [precioFinal, setPrecioFinal] = useState(() => {
+    const datosGuardados = localStorage.getItem("datos");
+    if (datosGuardados) {
+      const datos = JSON.parse(datosGuardados);
+      return datos.precioFinal;
+    }
+    return 0;
+  });
+  const [mostrarPanell, setMostrarPanell] = useState(() => {
+    const datosGuardados = localStorage.getItem("datos");
+    if (datosGuardados) {
+      const datos = JSON.parse(datosGuardados);
+      return datos.mostrarPanell;
+    }
+    return false;
+  });
+  const [nPaginas, setNPaginas] = useState(() => {
+    const datosGuardados = localStorage.getItem("datos");
+    if (datosGuardados) {
+      const datos = JSON.parse(datosGuardados);
+      return datos.nPaginas;
+    }
+    return 0;
+  });
+  const [nIdiomas, setNIdiomas] = useState(() => {
+    const datosGuardados = localStorage.getItem("datos");
+    if (datosGuardados) {
+      const datos = JSON.parse(datosGuardados);
+      return datos.nIdiomas;
+    }
+    return 0;
+  });
+
+  useEffect(() => {
+    const datosLocalStorage = {
+      precioFinal,
+      mostrarPanell,
+      nPaginas,
+      nIdiomas,
+    };
+    localStorage.setItem("datos", JSON.stringify(datosLocalStorage));
+  }, [precioFinal, mostrarPanell, nPaginas, nIdiomas]);
 
   const seleccionCasilla = (event) => {
     const valorCasilla = parseInt(event.target.value);
@@ -35,11 +73,39 @@ const Formulario = () => {
     setNIdiomas(newValue);
   };
 
+  useEffect(() => {
+    const datosGuardados = localStorage.getItem("checkboxes");
+    if (datosGuardados) {
+      const checkboxes = JSON.parse(datosGuardados);
+      for (const checkbox in checkboxes) {
+        document.getElementById(checkbox).checked = checkboxes[checkbox];
+      }
+    }
+  }, []);
+
+  const guardarCheckboxes = () => {
+    const checkboxes = {
+      checkbox1: document.getElementById("checkbox1").checked,
+      checkbox2: document.getElementById("checkbox2").checked,
+      checkbox3: document.getElementById("checkbox3").checked,
+    };
+    localStorage.setItem("checkboxes", JSON.stringify(checkboxes));
+  };
+
+  useEffect(() => {
+    guardarCheckboxes();
+  });
+
   return (
     <ContenedorFormulari>
       <p>¿Qué quieres hacer?</p>
       <label>
-        <input type="checkbox" value={500} onChange={seleccionCasilla} />
+        <input
+          id="checkbox1"
+          type="checkbox"
+          value={500}
+          onChange={seleccionCasilla}
+        />
         Una página web (500€)
       </label>
       {mostrarPanell && (
@@ -52,12 +118,22 @@ const Formulario = () => {
       )}
       <br />
       <label>
-        <input type="checkbox" value={300} onChange={seleccionCasilla} />
+        <input
+          id="checkbox2"
+          type="checkbox"
+          value={300}
+          onChange={seleccionCasilla}
+        />
         Una consultoría SEO (300€)
       </label>
       <br />
       <label>
-        <input type="checkbox" value={200} onChange={seleccionCasilla} />
+        <input
+          id="checkbox3"
+          type="checkbox"
+          value={200}
+          onChange={seleccionCasilla}
+        />
         Una campaña de Google Ads (200€)
       </label>
       <br />
